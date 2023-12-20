0d97a3df2524fb8d2d4408dc1d9072845968e206
twkwon0417 <kwontony123@konkuk.ac.kr>
2023-12-20 16:34:03 +0900

    feat: MemberService implementation

--- src/main/java/tony/side/domain/member/MemberService.java
+++ src/main/java/tony/side/domain/member/MemberService.java
@@ -1,4 +1,38 @@
 package tony.side.domain.member;
 
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public void join(Member member) {
+        memberRepository.save(member);
+    }
+
+    public Optional<Member> findByLoginIdAndPhoneNumber(String loginId, String phoneNumber) {
+        return memberRepository.findByLoginIdAndPhoneNumber(loginId, phoneNumber);
+    }
+
+    public void findById(long id) {
+        memberRepository.findById(id);
+    }
+
+    public void editMemberInfo(Long id, Member newMember) { // 아이디, 비밀번호는 못 바꾸지
+        Member member = memberRepository.findById(id);
+        member.setName(newMember.getName());
+        member.setName(newMember.getName());
+        member.setEMail(newMember.getEMail());
+        member.setBirthDate(newMember.getBirthDate());
+        member.setPhoneNumber(newMember.getPhoneNumber());
+    }
+
+    public void changePassword(Long id, String password) {     // parameter 바뀔 가능성 농후
+        Member member = memberRepository.findById(id);
+        member.setPassword(password);
+    }
 }
