3de43207520122b24a14bc114d374c472898ec45
twkwon0417 <kwontony123@konkuk.ac.kr>
2023-12-20 17:50:18 +0900

    fix: add Post Contructor

--- src/main/java/tony/side/domain/post/Post.java
+++ src/main/java/tony/side/domain/post/Post.java
@@ -11,4 +11,13 @@ public class Post {
     private Long userId;
     private String title;
     private String content;
+
+    public Post(Long userId, String title, String content) {
+        this.userId = userId;
+        this.title = title;
+        this.content = content;
+    }
+
+    public Post() {
+    }
 }
